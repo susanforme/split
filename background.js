c9685a6f0fe96dbc41f996652ecbfb5fd0b8fa38
Spring  Breeze <rzc307853639@gmail.com>
2022-02-08 18:50:56 +0800

    回滚到v2

--- background.js
+++ background.js
@@ -1,12 +1,19 @@
+const contextId = {
+  SPLIT1: "sampleContextMenu",
+};
 chrome.contextMenus.create({
-  title: "测试右键菜单",
-  onclick: function () {
-    alert("您点击了右键菜单！");
-  },
+  title: "分屏",
+  id: contextId.SPLIT1,
 });
 let color = "#3aa757";
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log("Default background color set to %c green", `color: ${color}`);
+chrome.contextMenus.onClicked.addListener((v) => {
+  const { pageUrl, menuItemId } = v;
+  switch (menuItemId) {
+    case contextId.SPLIT1:
+      break;
+    default:
+      break;
+  }
+  console.log(v);
+  console.log(pageUrl, menuItemId);
 });
