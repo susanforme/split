a4c71dabd3e445026b6c34992433101921ce756f
Spring  Breeze <rzc307853639@gmail.com>
2022-02-09 12:49:01 +0800

    chrome分屏

--- background.js
+++ background.js
@@ -1,14 +1,34 @@
 const contextId = {
   SPLIT1: "sampleContextMenu",
 };
+// [
+//   { id: "main", visible: true, title: "main" },
+//   { id: "sub1", visible: true, title: "sub1", parentId: "main" },
+// ];
 chrome.contextMenus.create({
   title: "分屏",
   id: contextId.SPLIT1,
+  visible: true,
 });
 chrome.contextMenus.onClicked.addListener((v) => {
   const { pageUrl, menuItemId } = v;
   switch (menuItemId) {
     case contextId.SPLIT1:
+      const urls = JSON.parse(localStorage.getItem("iframeUrl"));
+      let minUrlIndex = 0;
+      // 找出最久的url
+      for (let i = 1; i < urls.length; i++) {
+        if (urls[i].date < urls[i - 1].date) {
+          minUrlIndex = i;
+        }
+      }
+      urls[minUrlIndex] = {
+        url: pageUrl,
+        date: Date.now(),
+      };
+      localStorage.setItem("iframeUrl", JSON.stringify(urls));
+      const url = chrome.runtime.getURL("home.html");
+      chrome.tabs.create({ url });
       break;
     default:
       break;
@@ -21,9 +41,18 @@ function initUrl() {
   if (!localStorage.getItem("iframeUrl")) {
     localStorage.setItem(
       "iframeUrl",
-      JSON.stringify(["https://vuejs.org/", "https://reactjs.org/"])
+      JSON.stringify([
+        {
+          url: "https://vuejs.org/",
+          date: Date.now(),
+        },
+        {
+          url: "https://reactjs.org/",
+          date: Date.now() + 1,
+        },
+      ])
     );
   }
 }
-
+// https://github.com/frederickk/side-by-side
 initUrl();
--- js/home.js
+++ js/home.js
@@ -2,5 +2,6 @@ const iframes = document.querySelectorAll("iframe");
 const urls = JSON.parse(localStorage.getItem("iframeUrl"));
 for (let i = 0; i < iframes.length; i++) {
   const element = iframes[i];
-  element.src = urls[i];
+  element.id = urls[i].date;
+  element.src = urls[i].url;
 }
