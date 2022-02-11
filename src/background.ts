72a8d697baa92d6aec171750d9eafffc7b6d258f
Spring  Breeze <rzc307853639@gmail.com>
2022-02-11 19:01:58 +0800

    🐛 修复扩展页面套娃

--- src/background.ts
+++ src/background.ts
@@ -8,6 +8,7 @@ import { CONTEXT_ID } from "./utils/key";
 chrome.contextMenus.create({
   title: "分屏",
   id: CONTEXT_ID.SPLIT,
+  documentUrlPatterns: ["*://*/*"],
   visible: true,
 });
 chrome.contextMenus.onClicked.addListener((v) => {
@@ -25,3 +26,28 @@ chrome.contextMenus.onClicked.addListener((v) => {
   }
   console.log("当前页面为" + pageUrl);
 });
+
+// https://stackoverflow.com/questions/15532791/getting-around-x-frame-options-deny-in-a-chrome-extension
+chrome.webRequest.onHeadersReceived.addListener(
+  function (info) {
+    // 所有response头
+    const { responseHeaders } = info;
+    if (!responseHeaders) {
+      return;
+    }
+    for (let i = responseHeaders.length - 1; i >= 0; --i) {
+      const header = responseHeaders[i].name.toLowerCase();
+      if (header == "x-frame-options" || header == "frame-options") {
+        // Remove header
+        responseHeaders.splice(i, 1);
+      }
+    }
+    return { responseHeaders };
+  },
+  {
+    // Pattern to match all http(s) pages
+    urls: ["*://*/*"],
+    types: ["sub_frame"],
+  },
+  ["blocking", "responseHeaders"],
+);
